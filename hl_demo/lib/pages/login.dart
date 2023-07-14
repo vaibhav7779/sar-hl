d4622b76e692c8077b208c5feba55550c8aa299d
Shammy Raushan <shammyraushan@gmail.com>
2023-07-14 12:50:49 +0530

    Home Loan Demo setup

---
+++ hl_demo/lib/pages/login.dart
@@ -0,0 +1,165 @@
+import 'package:flutter/material.dart';
+// import 'package:flutter_breadcrumb/flutter_breadcrumb.dart';
+import 'package:step_progress_indicator/step_progress_indicator.dart';
+
+bool isChecked = false;
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home Loan"),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: Colors.white),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+        actions: <Widget>[
+          Icon(Icons.question_mark_rounded),
+          SizedBox(width: 15)
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // BreadCrumb(
+              //   items: <BreadCrumbItem>[
+              //     BreadCrumbItem(
+              //       content: const Text(
+              //         "HOME",
+              //         style:
+              //             TextStyle(fontSize: 10, fontWeight: FontWeight.w400),
+              //       ),
+              //     ),
+              //     BreadCrumbItem(
+              //       content: const Text(
+              //         "PERSONAL LOANS",
+              //         style:
+              //             TextStyle(fontSize: 10, fontWeight: FontWeight.w400),
+              //       ),
+              //     ),
+              //     BreadCrumbItem(
+              //       content: const Text(
+              //         "APPLY",
+              //         style:
+              //             TextStyle(fontSize: 10, fontWeight: FontWeight.w400),
+              //       ),
+              //     ),
+              //   ],
+              //   divider: const Icon(Icons.chevron_right),
+              // ),
+              const SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: const [Text("Verify Your Details"), Text("1/11")],
+              ),
+              const SizedBox(height: 10),
+              const StepProgressIndicator(
+                totalSteps: 11,
+                currentStep: 1,
+                selectedColor: Color(0xFF45C00B),
+              ),
+              const SizedBox(height: 40),
+              const Text(
+                "Enter your personal details",
+                style: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+              const SizedBox(height: 16),
+              Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15.0),
+                ),
+                elevation: 5,
+                child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      const Text(
+                        "Please enter accurate information that matches your KYC documents.",
+                        style: const TextStyle(
+                            fontSize: 14, fontWeight: FontWeight.w400),
+                      ),
+                      const SizedBox(height: 20),
+                      TextFormField(
+                        autofocus: false,
+                        decoration: const InputDecoration(
+                          labelText: "Your Name",
+                          hintText: "Enter Your Name",
+                          border: OutlineInputBorder(),
+                          filled: true, //<-- SEE HERE
+                          fillColor: Color(0xFFFFFFFF),
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      TextFormField(
+                        autofocus: false,
+                        decoration: const InputDecoration(
+                          labelText: "Mobile Number",
+                          hintText: "Enter Mobile Number",
+                          border: OutlineInputBorder(),
+                          filled: true, //<-- SEE HERE
+                          fillColor: Color(0xFFFFFFFF),
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      const Padding(
+                        padding: EdgeInsets.only(left: 8.0),
+                        child: Text(
+                            "*We will be sending an OTP to verify your details"),
+                      ),
+                      const SizedBox(height: 24),
+                      TextFormField(
+                        autofocus: false,
+                        decoration: const InputDecoration(
+                          labelText: "PAN",
+                          hintText: "Enter PAN",
+                          border: OutlineInputBorder(),
+                          filled: true, //<-- SEE HERE
+                          fillColor: Color(0xFFFFFFFF),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(height: 60),
+              Center(
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Container(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => LoginPage(),
+                            ));
+                      },
+                      child: const Text("Next"),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
